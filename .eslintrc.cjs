e7773c1d3ff97e807a54b5055375037893ead4b4
ti-pq <ti@p-req.com>
2023-03-31 16:11:03 +0900

    .eslintrc.cjs 修正

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,8 @@ module.exports = {
     "extends": [
         "eslint:recommended",
         "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:@typescript-eslint/recommended",
+        // "prettier"
     ],
     "overrides": [
     ],
@@ -18,8 +19,13 @@ module.exports = {
     },
     "plugins": [
         "react",
+        "react-hooks",
         "@typescript-eslint"
     ],
     "rules": {
+        "react/jsx-uses-react": "off",
+        "react/react-in-jsx-scope": "off",
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "warn"
     }
 }
